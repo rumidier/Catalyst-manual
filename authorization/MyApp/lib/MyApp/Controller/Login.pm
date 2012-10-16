ddb1c67145f8c2d5487f6c632d7a38d8492d1d74
Hanyoung Cho <rumidier@gmail.com>
2012-10-16 19:29:43 +0900

    Add authorization

---
+++ authorization/MyApp/lib/MyApp/Controller/Login.pm
@@ -0,0 +1,62 @@
+package MyApp::Controller::Login;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+MyApp::Controller::Login - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ($self, $c) = @_;
+
+    my $username = $c->request->params->{username};
+    my $password = $c->request->params->{password};
+
+	if ($username && $password) {
+
+		if ($c->authenticate({ username => $username,
+			password => $password  } )) {
+            	$c->response->redirect($c->uri_for(
+            	$c->controller('Books')->action_for('list')));
+                return;
+            } else {
+                $c->stash(error_msg => "Bad username or password.");
+            }
+        } else {
+            $c->stash(error_msg => "Empty username or password.")
+            unless ($c->user_exists);
+        }
+
+    $c->stash(template => 'login.tt2');
+}
+
+
+=head1 AUTHOR
+
+한조
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ authorization/MyApp/lib/MyApp/Controller/Logout.pm
@@ -0,0 +1,45 @@
+package MyApp::Controller::Logout;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+MyApp::Controller::Logout - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->logout;
+    $c->response->redirect($c->uri_for('/'));
+}
+
+
+=head1 AUTHOR
+
+한조
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
