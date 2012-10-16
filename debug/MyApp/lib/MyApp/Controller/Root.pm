34ca6725b82d5d0c1edaf3e4e699f835c220b3f5
Hanyoung Cho <rumidier@gmail.com>
2012-10-16 23:21:09 +0900

    Add debug

---
+++ debug/MyApp/lib/MyApp/Controller/Root.pm
@@ -0,0 +1,103 @@
+package MyApp::Controller::Root;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+#
+# Sets the actions in this controller to be registered with no prefix
+# so they function identically to actions created in MyApp.pm
+#
+__PACKAGE__->config(namespace => '');
+
+=head1 NAME
+
+MyApp::Controller::Root - Root Controller for MyApp
+
+=head1 DESCRIPTION
+
+[enter your description here]
+
+=head1 METHODS
+
+=head2 auto
+
+Check if there is a user and, if not, forward to login page
+
+=cut
+
+sub auto :Private {
+    my ($self, $c) = @_;
+
+    if ($c->controller eq $c->controller('Login')) {
+        return 1;
+    }
+
+    if (!$c->user_exists) {
+        $c->log->debug('***Root::auto User not found, forwarding to /login');
+        $c->response->redirect($c->uri_for('/login'));
+        return 0;
+    }
+
+    return 1;
+}
+
+=head2 index
+
+The root page (/)
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    # Hello World
+    $c->response->body( $c->welcome_message );
+}
+
+=head2 error_noperms
+
+Permissions error screen
+
+=cut
+
+sub error_noperms :Chained('/') :PathPart('error_noperms') :Args(0) {
+    my ($self, $c) = @_;
+
+    $c->stash(template => 'error_noperms.tt2');
+}
+
+=head2 default
+
+Standard 404 error page
+
+=cut
+
+sub default :Path {
+    my ( $self, $c ) = @_;
+    $c->response->body( 'Page not found' );
+    $c->response->status(404);
+}
+
+=head2 end
+
+Attempt to render a view, if needed.
+
+=cut
+
+sub end : ActionClass('RenderView') {}
+
+=head1 AUTHOR
+
+한조
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
