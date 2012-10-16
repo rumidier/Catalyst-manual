85534a94870b223b711a01552288b7f5be8d9729
Hanyoung Cho <rumidier@gmail.com>
2012-10-16 21:16:58 +0900

    admin 계정만 추가 할수 있게함

--- authorization/MyApp/lib/MyApp/Controller/Books.pm
+++ authorization/MyApp/lib/MyApp/Controller/Books.pm
@@ -62,31 +62,23 @@ Create a book with the supplied title, rating, and author
 =cut
 
 sub url_create :Chained('base') :PathPart('url_create') :Args(3) {
-    # In addition to self & context, get the title, rating, &
-    # author_id args from the URL.  Note that Catalyst automatically
-    # puts extra information after the "/<controller_name>/<action_name/"
-    # into @_.  The args are separated  by the '/' char on the URL.
     my ($self, $c, $title, $rating, $author_id) = @_;
 
-    # Call create() on the book model object. Pass the table
-    # columns/field values we want to set as hash values
-    my $book = $c->model('DB::Book')->create({
-            title  => $title,
-            rating => $rating
-        });
+    if ($c->check_user_roles('admin')) {
+        my $book = $c->model('DB::Book')->create({
+                title  => $title,
+                rating => $rating
+            });
 
-    # Add a record to the join table for this book, mapping to
-    # appropriate author
-    $book->add_to_book_authors({author_id => $author_id});
-    # Note: Above is a shortcut for this:
-    # $book->create_related('book_authors', {author_id => $author_id});
+        $book->add_to_book_authors( {author_id => $author_id} );
 
-    # Assign the Book object to the stash for display and set template
-    $c->stash(book     => $book,
-              template => 'books/create_done.tt2');
-
-    # Disable caching for this page
-    $c->response->header('Cache-Control' => 'no-cache');
+        $c->stash(
+            book     => $book,
+            template => 'books/create_done.tt2'
+        );
+    } else {
+        $c->response->body('Unauthorized!');
+    }
 }
 
 =head2 form_create
